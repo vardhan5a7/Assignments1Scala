4ebc5339249158a13416d1fd646651a33475e8e8
vardhan <vardhan5a7@gmail.com>
2021-12-09 22:40:08 +0530

    assignment1scala

--- src/main/scala/Question7.scala
+++ src/main/scala/Question7.scala
@@ -1,18 +1,22 @@
 class Question7 {
-  def checkStringHappyOrNot(string: String): String = {
+  def checkStringHappyOrNot(string: String, indexValue: Int): String = {
     var check = false
     if(string.isEmpty())
       return "The given string is null"
-    for(i <- 1 to string.length - 1)
-      if(string.charAt(i) == string.charAt(i - 1))
-        check = true
-
-    if(check == true)
-      "Happy"
+    if(indexValue > string.length)
+      return "choose correct index value"
+    if(indexValue >= 0 && indexValue <= string.length)
+      if(string.charAt(indexValue) == string.charAt(indexValue - 1))
+         "Happy"
+      else if(string.charAt(indexValue) == string.charAt(indexValue + 1))
+        "Happy"
+      else
+        "Not Happy"
     else
-      "Not Happay"
+      "Both condition"
   }
-  println(checkStringHappyOrNot("happy"))
-  println(checkStringHappyOrNot("malayalam"))
-  println(checkStringHappyOrNot(""))
+  println(checkStringHappyOrNot("happy", 2))
+  println(checkStringHappyOrNot("malayalam", 3))
+  println(checkStringHappyOrNot("" , 7))
+  println(checkStringHappyOrNot("developer", 17))
 }
--- src/test/scala/Question7Test.scala
+++ src/test/scala/Question7Test.scala
@@ -4,8 +4,9 @@ class Question7Test extends AnyFunSuite{
   val question7 = new Question7
 
   test(testName = "check the given string is happy or not happy string"){
-    assert(question7.checkStringHappyOrNot("happy") == "Happy")
-    assert(question7.checkStringHappyOrNot("malayalam") == "Not Happay")
-    assert(question7.checkStringHappyOrNot("") == "The given string is null")
+    assert(question7.checkStringHappyOrNot("happy",2) == "Happy")
+    assert(question7.checkStringHappyOrNot("malayalam", 3) == "Not Happy")
+    assert(question7.checkStringHappyOrNot("", 7) == "The given string is null")
+    assert(question7.checkStringHappyOrNot("developer", 17) == "choose correct index value")
   }
 }
